a50bfb8bc217b33a9aca8db237acd72454e334a6
abdullah-aljssar <160490932+abdullah-aljssar@users.noreply.github.com>
2026-08-09 02:21:16 +0300

    حذف الملفات القديمة

--- frontend/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    REALTIME_SERVER_URL: process.env.REALTIME_SERVER_URL || 'http://localhost:4000',
-  },
-};
-
-module.exports = nextConfig;
